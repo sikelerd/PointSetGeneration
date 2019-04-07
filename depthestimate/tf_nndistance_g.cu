c10fd93d2a575c9927539e17e06aba8a7122581a
Daniel Sikeler <sikeler@hm.edu>
2019-04-07 18:32:58 +0200

    synchronize

--- depthestimate/tf_nndistance_g.cu
+++ depthestimate/tf_nndistance_g.cu
@@ -128,6 +128,7 @@ __global__ void NmDistanceKernel(int b,int n,const float * xyz,int m,const float
 void NmDistanceKernelLauncher(int b,int n,const float * xyz,int m,const float * xyz2,float * result,int * result_i,float * result2,int * result2_i){
 	NmDistanceKernel<<<dim3(32,16,1),512>>>(b,n,xyz,m,xyz2,result,result_i);
 	NmDistanceKernel<<<dim3(32,16,1),512>>>(b,m,xyz2,n,xyz,result2,result2_i);
+	cudaDeviceSynchronize();
 }
 __global__ void NmDistanceGradKernel(int b,int n,const float * xyz1,int m,const float * xyz2,const float * grad_dist1,const int * idx1,float * grad_xyz1,float * grad_xyz2){
 	for (int i=blockIdx.x;i<b;i+=gridDim.x){
@@ -154,6 +155,7 @@ void NmDistanceGradKernelLauncher(int b,int n,const float * xyz1,int m,const flo
 	cudaMemset(grad_xyz2,0,b*m*3*4);
 	NmDistanceGradKernel<<<dim3(1,16,1),256>>>(b,n,xyz1,m,xyz2,grad_dist1,idx1,grad_xyz1,grad_xyz2);
 	NmDistanceGradKernel<<<dim3(1,16,1),256>>>(b,m,xyz2,n,xyz1,grad_dist2,idx2,grad_xyz2,grad_xyz1);
+	cudaDeviceSynchronize();
 }
 
 #endif
