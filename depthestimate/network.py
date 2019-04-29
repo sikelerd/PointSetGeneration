1960e42f9a05d124c0cfae9ba9a6684ad495445c
Daniel Sikeler <sikeler@hm.edu>
2019-04-29 18:18:55 +0200

    force points on scan lines

--- depthestimate/network.py
+++ depthestimate/network.py
@@ -1,6 +1,7 @@
 import tflearn
 import tensorflow as tf
 from . import tf_nndistance
+import numpy as np
 
 
 class MappingNetwork:
@@ -11,10 +12,26 @@ class MappingNetwork:
         }
         if training:
             self._placeholders['pc_gt'] = tf.placeholder(tf.float32, shape=(None, 8000, 3), name='pt_gt')
-        self.distributed_points = 256
-        self.smoothed_points = 768
+        self.distributed_points = int(256 * 3 / 2)
+        self.smoothed_points = int(768 * 3 / 2)
         self.num_points = self.smoothed_points + self.distributed_points
+        print(self.distributed_points, self. smoothed_points, self.num_points)
         self.factor = factor
+
+        p_start = np.pi / 4
+        p_end = np.pi * 3 / 4
+        p_channels = 96
+        p = np.linspace(p_start, p_end, p_channels)
+
+        t_start = -0.4677
+        t_end = 0.4677
+        t_channels = 64
+        t = np.linspace(t_start, t_end, t_channels)
+
+        x_koor = np.tile(np.sin(p), 64)
+        y_koor = np.tile(np.cos(p), 64)
+        z_koor = np.repeat(1 - np.cos(t), 96)
+        self.koordinates = np.transpose(np.array([x_koor, y_koor, z_koor]), (1, 0))
 
     @property
     def placeholders(self):
@@ -23,10 +40,10 @@ class MappingNetwork:
 
     def build_net(self, image_current):
         tflearn.init_graph(seed=1029, num_cores=2, gpu_memory_fraction=0.9, soft_placement=True)
-        batch_size = tf.shape(image_current)[0]
 
         with tf.variable_scope('points', reuse=tf.AUTO_REUSE):
             x = image_current
+            batch_size = tf.shape(x)[0]
             # 240 320
             img = tf.image.resize_images(x, (192, 256))
             points = []
@@ -142,21 +159,28 @@ class MappingNetwork:
                 x = tflearn.layers.conv.conv_2d(x, 64, (3, 3), strides=1, activation='relu', weight_decay=1e-5, regularizer='L2')
 
                 x_additional = tflearn.layers.core.fully_connected(x_additional, 1024, activation='relu', weight_decay=1e-3, regularizer='L2')
-                x_additional = tflearn.layers.core.fully_connected(x_additional, self.distributed_points * 3, activation='linear', weight_decay=1e-3, regularizer='L2')
-                x_additional = tf.reshape(x_additional, (batch_size, self.distributed_points, 3))
+                x_additional = tflearn.layers.core.fully_connected(x_additional, 256 * 3, activation='linear', weight_decay=1e-3, regularizer='L2')
+                x_additional = tf.reshape(x_additional, (batch_size, self.distributed_points, 2))
                 x = tflearn.layers.conv.conv_2d(x, 3, (3, 3), strides=1, activation='linear', weight_decay=1e-5, regularizer='L2')
-                x = tf.reshape(x, (batch_size, self.smoothed_points, 3))
+                x = tf.reshape(x, (batch_size, self.smoothed_points, 2))
                 x = tf.concat([x_additional, x], 1)
-                x = tf.reshape(x, (batch_size, self.num_points, 3), name='points')
+                x = tf.reshape(x, (batch_size, self.num_points, 2), name='points')
                 points.append(x)
+
             p = tf.concat(points, axis=1)
-            print(p)
-        return p
+            koor = tf.constant(self.koordinates, dtype=tf.float32)
+
+            r = tf.reshape(p[:, :, 0], (-1, self.num_points * self.factor, 1))
+            h = tf.reshape(p[:, :, 1], (-1, self.num_points * self.factor, 1))
+            i = tf.concat([r, r, h], 2)
+
+            koor = tf.multiply(koor, i, name='points')
+        return koor
 
     def build_training_net(self, image_current, pc_gt):
         x = self.build_net(image_current)
         dists_forward, idx_forward, dists_backward, idx_backward = tf_nndistance.nn_distance(pc_gt, x)
-        # dists_forward = dists_forward * tf.norm(pc_gt, axis=2)
+        dists_forward = dists_forward * tf.norm(pc_gt, axis=2)
         mindist_forward = dists_forward
         mindist_backword = dists_backward
         dists_forward = tf.reduce_mean(dists_forward)
